53de6588ec8f9bdf38200b36f946ead6b48a9b62
AJ Webb <webbushka@gmail.com>
2023-07-20 21:52:03 -1000

    solution

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let firstArg = "";
-let secondArg = "";
+let firstArg = '';
+let secondArg = '';
 let operator = null;
 
 function add(num1, num2) {
@@ -17,3 +17,57 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function repaint() {
+  document.getElementById('display').value = secondArg;
+}
+
+function doMath() {
+  secondArg = `${operator(+firstArg, +secondArg)}`;
+}
+
+function operatorPushed(op) {
+  if (firstArg) {
+    doMath();
+  }
+
+  firstArg = secondArg;
+  secondArg = '';
+  operator = op;
+}
+
+function btnPush(evt) {
+  const btn = evt.target.dataset.btn;
+  let canDoMath = !!firstArg;
+  switch (btn) {
+    case 'x':
+      operatorPushed(multiply);
+      break;
+    case '/':
+      operatorPushed(divide);
+      break;
+    case '+':
+      operatorPushed(add);
+      break;
+    case '-':
+      operatorPushed(subtract);
+      break;
+    case '=':
+      doMath();
+      break;
+    case 'del':
+      firstArg = '';
+      secondArg = '';
+      operator = null;
+      break;
+    default:
+      secondArg += btn;
+      break;
+  }
+
+  repaint();
+}
+
+document
+  .querySelectorAll('.btn')
+  .forEach((btn) => btn.addEventListener('click', btnPush));
